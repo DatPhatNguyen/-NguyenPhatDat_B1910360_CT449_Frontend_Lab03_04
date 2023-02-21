ee1a179cd36fc6171cffb59b18595f4eae96d01c
DatPhatNguyen <phatdat040801@gmail.com>
2023-02-21 14:44:41 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+  export default {
+
+  }
+</script>
+
+<template>
+  <header>
+      <h1>Hello, vue.js!</h1>
+  </header>
+</template>
+
+<style >
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
